5b74c4ce02d92a174fe6e0560c52694ace47d404
Santiago Quintero <santiago.qp86@gmail.com>
2023-07-27 23:38:23 -0500

    hw03-mongodb

---
+++ service/schemas/contact.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Joi = require("joi");
+
+// Joi
+const validationContact = Joi.object({
+    name: Joi.string()
+      .min(2)
+      .max(35)
+      .pattern(/^[A-Za-z ]+$/)
+      .messages({
+        "string.pattern.base":
+        "Invalid name. The name must be written only in letters"
+      }),
+    email: Joi.string()
+      .email({ minDomainSegments: 2})
+      .pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)
+      .messages({
+        "string.pattern.base":"Invalid email"
+      }),
+    phone: Joi.string()
+      .min(7)
+      .max(30)
+      .pattern( /^(?:\+?\d{1,2}\s)?[\d\s()\-\+]+$/)
+      .messages({
+        "string.pattern.base":"Invalid phone"
+      }),
+      favorite: Joi.boolean()
+})
+
+// Mongoose Schema
+
+const contact = new Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 35,
+    match: /^[A-Za-z ]+$/,
+    unique: true,
+    require: [true, "Name is required"],
+  },
+  email: {
+    type: String,
+    match: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    unique: true,
+    require: [true, "Email is required"],
+  },
+  phone: {
+    type: String,
+    match: /^(?:\+?\d{1,2}\s)?[\d\s()\-\+]+$/,
+    unique: true,
+    require: [true, "Phone is required"],
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+    require: [true]
+  },
+  },
+  {versionKey: false}
+)
+
+const Contact = mongoose.model("contact", contact);
+module.exports = { validationContact, Contact };
